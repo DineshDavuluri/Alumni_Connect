a847d7a68f278411f57e0f15aec2f609bccb5979
DineshDavuluri <DineshDavuluri@gmail.com>
2025-05-22 20:01:27 +0530

    update

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,7 +1,12 @@
-export type Post = {
-  id: string;
-  title: string;
-  content: string;
-  createdAt: Date;
-  authorId: string;
-};
+import mongoose from "mongoose";
+import { string } from "zod";
+
+const UpdateSchema = new mongoose.Schema({
+  id: string,
+  title: string,
+  content: string,
+  createdAt: Date,
+  authorId: string,
+});
+
+export default mongoose.models.Update || mongoose.model("Post", UpdateSchema);
